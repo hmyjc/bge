1ea0610bb5fac4b597a73cbf53263c44edbfac28
hmyjc <1768225144@qq.com>
2025-02-10 00:05:25 +0800

    Direct push from Kaggle

--- code/.ipynb_checkpoints/bge_model-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import (
-    SentenceTransformer,
-    SentenceTransformerTrainer,
-    SentenceTransformerTrainingArguments,
-)
-def get_model(cfg):
-    model = SentenceTransformer(cfg.MODEL_    NAME)
